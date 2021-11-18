cc33aa27aa74f22dd7f0e55bad6a60115fb733cd
Lenzenis <lenzenis@msu.edu>
2021-11-18 18:35:15 -0500

    commit

---
+++ soundboard.js
@@ -0,0 +1,31 @@
+var getchampagne = document.getElementById(champagnesound)
+var getcough = document.getElementById(coughsound)
+var getwind = document.getElementById(windsound)
+var getblip = document.getElementById(blipsound)
+
+function champagne() {
+    getchampagne.play();
+
+}
+
+function cough() {
+    getcough.play();
+
+}
+
+function wind() {
+    getwind.play();
+
+}
+function champagneenter() {
+    getblip.play();
+
+}
+function coughenter() {
+    getblip.play();
+
+}
+function windenter() {
+    getblip.play();
+
+}
